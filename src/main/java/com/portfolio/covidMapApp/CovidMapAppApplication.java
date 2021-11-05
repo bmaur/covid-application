bc576065bd5aca1949965daad4bd57fbece133dd
bmaur <bartekmaur@interia.pl>
2021-11-05 10:16:44 +0100

    settings

--- src/main/java/com/portfolio/covidMapApp/CovidMapAppApplication.java
+++ src/main/java/com/portfolio/covidMapApp/CovidMapAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CovidMapAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CovidMapAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CovidMapAppApplication.class, args);
+    }
 
 }
